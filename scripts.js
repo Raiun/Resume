f79929d045c1345e44c05c7525da049ce772c010
Raiun <ryan.man821@gmail.com>
2023-07-27 10:18:59 -0700

    Formatted grid items into cards + moved expand button into grid title area + cleaned up some css

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 //Function to open or close additional content in details
-function expand() {
+function expand(type) {
     document.body.querySelectorAll('details')
     .forEach((e) => {(e.hasAttribute('open')) ? e.removeAttribute('open') : e.setAttribute('open', true);})
+    /*const elements = document.getElementsByClassName(type)
+    for (let i = 0; i < document.length; i++) {
+        (elements[i].hasAttribute("open")) ? element.removeAttribute("open") : element.hasAttribute("open", true);
+    }*/
 }
