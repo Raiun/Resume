417cafb8c55da9ed5f7d6a66f1abbb31a46821d7
Raiun <ryan.man821@gmail.com>
2023-08-08 18:26:35 -0700

    Revised popup system (working open and close now), and began to add dynamic rendering for popup

--- scripts.js
+++ scripts.js
@@ -22,18 +22,46 @@ function expand(type) {
 }
 
 function popup(card) {
-    //(document.body.style.filter == "none") ? document.body.style.filter = "blur(4px)" : document.body.style.filter = "none";
-    document.getElementById("content").classList.toggle("darken");
-    let popup = document.getElementById(card);
+    let popup = document.getElementsByClassName("popup")[0];
     console.log(card)
     popup.classList.toggle("show");
-    console.log(popup)
-}
+    let overlay = document.getElementById("overlay"); 
+    overlay.style.display = (overlay.style.display == "none") ? "block" : "none";
 
-function exitPopup() {
-    blurEffect = document.body.style.filter;
-    console.log(blurEffect);
-    if (blurEffect == "blur(4px)") {
-        document.body.style.filter = "none";
+    switch(card) {
+        case "w1":
+            title = popup.querySelector("p");
+            title.innerHTML = "Pullscription: Software Engineering Intern"
+            break;
+        case "w2":
+            title = popup.querySelector("p");
+            title.innerHTML = "UCLA IT Services: IT Student Consultant"
+            break;
+        case "w3":
+            title = popup.querySelector("p");
+            title.innerHTML = "UCLA Student Media: DevOps Intern"
+            break;
+        case "w4":
+            title = popup.querySelector("p");
+            title.innerHTML = "Albany's Landmark Twin: Floor Staff"
+            break;
+        case "p1":
+            title = popup.querySelector("p");
+            title.innerHTML = "Bruin-Study"
+            break;
+        case "p2":
+            title = popup.querySelector("p");
+            title.innerHTML = "Twitter Sentiment Tracker"
+            break;
+        case "p3":
+            title = popup.querySelector("p");
+            title.innerHTML = "Automotive Pilot State Recognition"
+            break;
+        case "p4":
+            title = popup.querySelector("p");
+            title.innerHTML = "UCLA Institute of Electrical Engineering: Micromouse Program"
+            break;
+        default:
+            break;
     }
 }
